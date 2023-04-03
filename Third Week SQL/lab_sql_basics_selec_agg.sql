8c5d6f22011f395b794f24e4772ef11d4750c800
jagiussani <jagiussani@gmail.com>
2023-04-03 16:34:11 +0200

    Update_finished the obligatory part

--- Third Week SQL/lab_sql_basics_selec_agg.sql
+++ Third Week SQL/lab_sql_basics_selec_agg.sql
@@ -63,7 +63,35 @@ WHERE account_id = 34
 
 ####### Query 10 #####
 # In the order table, which account_ids were responsible for orders between order_id 29540 and order_id 29560 (inclusive)?
-SELECT order_id, account_id FROM `order`
-WHERE  29540 => order_id => 29560; 
+SELECT DISTINCT (account_id) FROM `order`
+WHERE order_id BETWEEN 29540 AND 29560; 
+
+####### Query 11 #####
+# In the order table, what are the individual amounts that were sent to (account_to) id 30067122?
+SELECT amount FROM `order`
+WHERE account_to = 30067122;
+
+####### Query 12 #####
+# In the trans table, show the trans_id, date, 
+# type and amount of the 10 first transactions 
+# from account_id 793 in chronological order, from newest to oldest.
+
+SELECT trans_id, `date`,`type`, amount FROM trans
+WHERE account_id = 793
+ORDER BY `date` DESC
+LIMIT 10; 
+
+####### Query 13 #####
+# In the client table, of all districts with a district_id lower than 10, 
+# how many clients are from each district_id? 
+
+# Show the results sorted by the district_id in ascending order.
+
+SELECT district_id, count(client_id) FROM client
+WHERE district_id < 10
+GROUP BY client_id
+ORDER BY district_id ASC;
+
+
 
 
